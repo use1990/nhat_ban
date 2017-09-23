941c25a7ce2666de45a3856a3c0755267f9deeb7
use1990 <32218452+use1990@users.noreply.github.com>
2017-09-23 19:00:35 +0900

    Add files via upload

---
+++ index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+
+<script type="text/javascript" src="js/jquery.1.7.2.min.js"></script>
+<script type="text/javascript" src="js/ajax_xuly.js"></script>
+<script type="text/javascript" src="bootrap/js/bootstrap.js"></script>
+<script type="text/javascript" src="bootrap/js/bootstrap.min.js"></script>
+<link href="bootrap/css/bootstrap.css.css" rel="stylesheet" type="text/css" />
+<link href="css/rieng.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+<!-- top ---------------------------->
+
+<div class="top_top"></div>
+<!-- end top-------------------------- -->
+
+<!-- body  ------------------------ -->
+<div class="body_bg"></div>
+<div class="khung_giua">
+	<div class="bong_do"><div class="bong_do2">
+    <div class="dinh_dang">
+<!-- ban nenbody --------------->    
+    <div class="baner_body"></div>
+    	<div class="container">
+        <div class="row">
+        	<div class="col-md-3"><div class="row">Đặc điểm nổi bật:
+
+Bộ tiền xử lý: Ngoài vanilla CSS, Bootstrap còn hỗ trợ hai bộ tiền xử lý CSS phổ biến nhất là Less và Sass.
+Responsive across devices
+Một framework, mọi thiết bị: Bootstrap dễ dàng tích hợp và hiệu quả với quy mô dự án của bạn, Với một bộ mã nguồn căn bản, từ mobile đến tablet đến desktop.
+Tài liệu đầy đủ: Với Bootstrap, bạn sẽ có được tài liệu phong phú và trong sáng với hàng trăm ví dụ, code mẫu, và nhiều hơn nữa.
+Sau đây là một thiết kế đầu tiên:
+
+Bước 1: Tải bộ Bootstrap .Phiên bản hiện tại là 3.1.1
+
+Có một số cách rất đơn giản để bắt đầu với Bootstrap (phiên bản hiện tại v3.1.1), mỗi cách phù hợp với các trình độ và các nhu cầu khác nhau. Hãy đọc qua và xem cách nào phù hợp với nhu cầu cụ thể của bạn.
+
+Bước 2: Giải nén bộ Bootstrap đã tải về
+
+Khi tải về, hãy giải nén thư mục và bạn sẽ nhìn thấy cấu trúc của Bootstrap (đã biên dịch) tương tự như thế này:
+
+Cấu trúc thư mục bên trong gồm có:</div></div>
+            <div class="col-md-9">Đặc điểm nổi bật:
+
+Bộ tiền xử lý: Ngoài vanilla CSS, Bootstrap còn hỗ trợ hai bộ tiền xử lý CSS phổ biến nhất là Less và Sass.
+Responsive across devices
+Một framework, mọi thiết bị: Bootstrap dễ dàng tích hợp và hiệu quả với quy mô dự án của bạn, Với một bộ mã nguồn căn bản, từ mobile đến tablet đến desktop.
+Tài liệu đầy đủ: Với Bootstrap, bạn sẽ có được tài liệu phong phú và trong sáng với hàng trăm ví dụ, code mẫu, và nhiều hơn nữa.
+Sau đây là một thiết kế đầu tiên:
+
+Bước 1: Tải bộ Bootstrap .Phiên bản hiện tại là 3.1.1
+
+Có một số cách rất đơn giản để bắt đầu với Bootstrap (phiên bản hiện tại v3.1.1), mỗi cách phù hợp với các trình độ và các nhu cầu khác nhau. Hãy đọc qua và xem cách nào phù hợp với nhu cầu cụ thể của bạn.
+
+Bước 2: Giải nén bộ Bootstrap đã tải về
+
+Khi tải về, hãy giải nén thư mục và bạn sẽ nhìn thấy cấu trúc của Bootstrap (đã biên dịch) tương tự như thế này:
+
+Cấu trúc thư mục bên trong gồm có:</div>
+        	Đặc điểm nổi bật:
+
+Bộ tiền xử lý: Ngoài vanilla CSS, Bootstrap còn hỗ trợ hai bộ tiền xử lý CSS phổ biến nhất là Less và Sass.
+Responsive across devices
+Một framework, mọi thiết bị: Bootstrap dễ dàng tích hợp và hiệu quả với quy mô dự án của bạn, Với một bộ mã nguồn căn bản, từ mobile đến tablet đến desktop.
+Tài liệu đầy đủ: Với Bootstrap, bạn sẽ có được tài liệu phong phú và trong sáng với hàng trăm ví dụ, code mẫu, và nhiều hơn nữa.
+Sau đây là một thiết kế đầu tiên:
+
+Bước 1: Tải bộ Bootstrap .Phiên bản hiện tại là 3.1.1
+
+Có một số cách rất đơn giản để bắt đầu với Bootstrap (phiên bản hiện tại v3.1.1), mỗi cách phù hợp với các trình độ và các nhu cầu khác nhau. Hãy đọc qua và xem cách nào phù hợp với nhu cầu cụ thể của bạn.
+
+Bước 2: Giải nén bộ Bootstrap đã tải về
+
+Khi tải về, hãy giải nén thư mục và bạn sẽ nhìn thấy cấu trúc của Bootstrap (đã biên dịch) tương tự như thế này:
+
+Cấu trúc thư mục bên trong gồm có:
+</div>
+</div>
+</div>
+    </div>
+    </div>
+    <div class="duoi_bner">
+</div>
+</div></div></div>
+</div>
+
+<div class="botom"></div>
+</body>
+</html>
